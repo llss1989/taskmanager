e32fbfa7156d9ebb2f030fe0a2301285a0c82037
llss1989 <argenkasymov2@gmail.com>
2022-12-13 19:56:02 +0600

    mailer for create task added

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+  default from: 'noreply@taskmanager.com'
+    
+    def task_created
+      user = params[:user]
+      @task = params[:task]
+      
+      mail(to: user.email, subject: 'New Task Created')
+    end
+
+  end
